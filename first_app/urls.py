e2ac6edfe7ec2de71744d6c0f7ffc2415efcc7d8
Partha Bora <Partha.Bora@marlabs.com>
2022-05-31 21:17:59 +0530

    fourth commit

--- first_app/urls.py
+++ first_app/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     #localhost:8000/first_app/
-    path('', views.simple_view)
+    # path('', views.simple_view)
+
+
+    #localhost:8000/first_app/sports/
+    # path('sports/',views.sports_view),
+    #localhost:8000/first_app/finance/
+    # path('finance/', views.finance_view)
+
+    path('<topic>/', views.news_views)
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -2,5 +2,22 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 
 # Create your views here.
-def simple_view(request):
-    return HttpResponse("SIMPLE VIEW") #JINJA  html template  
+# def simple_view(request):
+#     return HttpResponse("SIMPLE VIEW") #JINJA  html template  
+
+
+articles = {
+    'sports':'Sports Page',
+    'finance':'Finance Page',
+    'politics':'Politics Page'
+}
+
+def news_views(request, topic):
+    return HttpResponse(articles[topic])
+
+# def sports_view(request):
+#     return HttpResponse(articles['sports']) #JINJA  html template  
+
+# def finance_view(request):
+#     return HttpResponse(articles['finance']) #JINJA  html template  
+ 
