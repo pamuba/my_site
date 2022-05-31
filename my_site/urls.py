f15be55f16a0103d4a74937cc8bccea7d0a5dd19
Partha Bora <Partha.Bora@marlabs.com>
2022-05-31 20:49:06 +0530

    third commit

--- my_site/urls.py
+++ my_site/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #localhost:800/first_app/
-    path('first_app/', include('first_app.urls'))
+    #localhost:8000/first_app/
+    path('first_app/', include('first_app.urls')),
+    #localhost:8000/
+    path('', views.home_view)
 ]
